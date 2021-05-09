33e203cc58084a0fc3866e8b047521482232c43e
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-09 14:09:02 +0100

    added inquire prompt to collect manager details and hooked up with generateHTML function

--- index.js
+++ index.js
@@ -5,13 +5,41 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const generateHTML = require("./src/utils/generateHTML");
 
-const generateManager = () => {};
-const generateMenu = () => {};
+const generateManager = async () => {
+  const managerQuestions = [
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the manager's name:",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "Enter the manager's employee ID:",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Enter the manager's e-mail address:",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "Enter the manager's office number:",
+    },
+  ];
 
-const init = () => {
-  generateManager();
-  generateMenu();
-  generateHTML.generateHTML();
+  const managerObject = await inquirer.prompt(managerQuestions);
+
+  return managerObject;
+};
+
+const generateEmployees = () => {};
+
+const init = async () => {
+  const manager = await generateManager();
+  const employees = generateEmployees();
+  generateHTML(manager, employees);
 };
 
 init();
---
+++ src/utils/generateHTML.js
@@ -0,0 +1,6 @@
+const generateHTML = (managerObject, employeeObject) => {
+  console.log(managerObject);
+  console.log(employeeObject);
+};
+
+module.exports = generateHTML;

08ed1eb023e5e612b7663a8f11970ce742452434
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-09 13:13:09 +0100

    added test to Employee getName method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,26 @@
+const { expect, test } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
   const name = "Bob";
   const id = 1;
   const email = "bob@company.co.uk";
+  const bob = new Employee(name, id, email);
 
   test("nem Employee instance's name should be set to passed in argument", () => {
-    const bob = new Employee(name, id, email);
     expect(bob.name).toBe(name);
   });
 
   test("nem Employee instance's id should be set to passed in argument", () => {
-    const bob = new Employee(name, id, email);
     expect(bob.id).toBe(id);
+  });
+
+  test("nem Employee instance's email should be set to passed in argument", () => {
+    expect(bob.id).toBe(id);
+  });
+
+  test("getName method should return employee's name", () => {
+    const bobName = bob.getName();
+    expect(bobName).toBe(name);
   });
 });
